26d81be7925c06f5d90961ca64150b06e29c7a0e
saba1926 <gogoladesaba@gmail.com>
2025-05-06 15:28:29 +0400

    commit

---
+++ day24/classwork/task1.py
@@ -0,0 +1,14 @@
+cars = ["bmw" , "mercedes" , "audi" , "opel" , "nisan" , "infininty" , "jeep" , "mustang" , "ferrary" , "proche"] 
+
+#1 
+print(cars[2]) 
+#2
+cars[0] = "opel" 
+#3
+print(cars[2:5]) 
+#4 
+print(cars[2::]) 
+#5
+print(cars[:5]) 
+#6 
+print(cars[::-1])
